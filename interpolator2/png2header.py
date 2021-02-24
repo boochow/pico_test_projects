108d931a318395c73363869df75703642d41d9c2
hayakawa <boochowp@gmail.com>
2021-02-24 23:02:14 +0900

    add png2header.py which generates map data file from a png file.

---
+++ interpolator2/png2header.py
@@ -0,0 +1,37 @@
+import os, sys, math
+from PIL import Image
+import numpy as np
+
+def is_power_of_two(x):
+    return (x & (x - 1)) == 0
+
+if len(sys.argv) != 2:
+    sys.stderr.write("png2header imagefile\n")
+    sys.exit(1)
+
+if not os.path.exists(sys.argv[1]):
+    sys.stderr.write(sys.argv[1] + " not found\n")
+    sys.exit(1)
+
+im = Image.open(sys.argv[1])
+if not(is_power_of_two(im.size[0]) and is_power_of_two(im.size[1])):
+    sys.stderr.write("width and height of the image must be 2^x\n")
+    sys.exit(1)
+
+print("constexpr uint8_t map_width  = ", end='')
+print(im.size[0].bit_length() - 1, end='')
+print(";")
+
+print("constexpr uint8_t map_height = ", end='')
+print(im.size[1].bit_length() - 1, end='')
+print(";\n")
+
+indexed = np.array(im)
+print('constexpr uint8_t map[] = \n{')
+for l in indexed:
+    print("\t", end='')
+    for p in l:
+        print(p,', ', sep='', end='')
+    print()
+print('};')
+
